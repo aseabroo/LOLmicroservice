b4b113edd9805efd5bb8ac3c27c5a470fa93f3f3
Augustus Seabrooke <august.seabrooke@gmail.com>
2023-11-20 22:07:37 -0500

    add pic

--- LOLmicroservice.py
+++ LOLmicroservice.py
@@ -9,7 +9,7 @@ def fetch_champion_data():
     Fetches the data of League of Legends champions from the Riot Games API.
 
     Returns:
-        dict: A dictionary mapping champion names to their respective image URLs.
+        dict: A dictionary containing champion data.
         str: Error message in case of a failure to fetch data.
     """
     url = "https://ddragon.leagueoflegends.com/cdn/13.23.1/data/en_US/champion.json"
@@ -18,7 +18,15 @@ def fetch_champion_data():
     if response.status_code == 200:
         data = response.json()
         champions = {
-            data['data'][champ]['name']: f"http://ddragon.leagueoflegends.com/cdn/13.23.1/img/champion/{champ}.png"
+            champ: {
+                "name": data['data'][champ]['name'],
+                "image": f"http://ddragon.leagueoflegends.com/cdn/13.23.1/img/champion/{champ}.png",
+                "sprite": data['data'][champ]['image']['sprite'],
+                "x": data['data'][champ]['image']['x'],
+                "y": data['data'][champ]['image']['y'],
+                "w": data['data'][champ]['image']['w'],
+                "h": data['data'][champ]['image']['h']
+            }
             for champ in data['data']
         }
         return champions
@@ -28,17 +36,17 @@ def fetch_champion_data():
 @app.route('/random-champion', methods=['GET'])
 def random_champion():
     """
-    Endpoint to get a random League of Legends champion's name and image URL.
+    Endpoint to get a random League of Legends champion's data.
 
     Returns:
-        JSON: A JSON object containing the champion's name and image URL.
+        JSON: A JSON object containing the champion's data.
     """
     champion_data = fetch_champion_data()
     if champion_data and isinstance(champion_data, dict):
-        champion_name, champion_image_url = random.choice(list(champion_data.items()))
-        return jsonify({"champion": champion_name, "image": champion_image_url})
+        champion_key, champion_info = random.choice(list(champion_data.items()))
+        return jsonify(champion_info)
     else:
         return jsonify({"error": "Unable to fetch champion data"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)  # The application will run on http://localhost:5000
+    app.run(debug=True, port=5000)
--- testMicroservice.py
+++ testMicroservice.py
@@ -1,15 +1,32 @@
 import requests
+import webbrowser
 
-def get_random_champion():
-    url = 'http://127.0.0.1:5000/random-champion'
-    response = requests.get(url)
+def fetch_random_champion():
+    """
+    Fetches a random League of Legends champion's data from the microservice.
 
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        return "Error: Unable to fetch data from the microservice"
+    Returns:
+        dict: A dictionary containing champion data.
+        None: If there was an error in fetching data.
+    """
+    try:
+        response = requests.get('http://127.0.0.1:5000/random-champion')
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print("Failed to fetch data from microservice")
+            return None
+    except requests.RequestException as e:
+        print(f"An exception occurred: {e}")
+        return None
 
-# Example usage
-champion_data = get_random_champion()
-print(champion_data)
+def main():
+    champion_data = fetch_random_champion()
+    if champion_data and 'image' in champion_data:
+        image_url = champion_data['image']
+
+        # Open the image in the default web browser
+        webbrowser.open(image_url)
+
+if __name__ == '__main__':
+    main()
